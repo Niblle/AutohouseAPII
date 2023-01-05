7ae513695652c9382e6ffbad9c643b6bd657b027
Tihomir Tsvetkov <S2109010788@sd.uni-vt.bg>
2023-01-05 10:57:23 +0200

    CSV helper

---
+++ Services/AutoHouseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AutoHouseModel
+    {
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? VehicleType { get; set; }
+        public double? EngineSize { get; set; }
+        public double? Horsepower { get; set; }
+    }
+}
---
+++ Services/CSVService.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace Services
+{
+    public class CSVService: ICSVService
+    {
+        public List<AutoHouseModel> ReadTradesFromFile(string filename)
+        {
+            
+            using (var reader = new StreamReader(filename))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AutoHouseModel>().ToList();
+                return records;
+            }
+
+        }
+    }
+}
